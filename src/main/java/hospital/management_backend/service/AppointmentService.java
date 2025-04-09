9737108ec2ec9987b393a267b9741f04efdb5d52
aadil792 <aadilmd792@gmail.com>
2025-04-09 11:09:49 +0530

    appointment crud operation

--- src/main/java/hospital/management_backend/service/AppointmentService.java
+++ src/main/java/hospital/management_backend/service/AppointmentService.java
@@ -1,7 +1,6 @@
 package hospital.management_backend.service;
 
 import hospital.management_backend.model.Appointment;
-import hospital.management_backend.repository.AppointmentRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +10,6 @@ public interface AppointmentService {
     Appointment create (Appointment appointment);
     void deleteAppointmentById (Long id);
     List<Appointment> getAllList();
-
+    Appointment update (Long id ,Appointment appointmentDetails);
+    Optional<Appointment> getById (Long id);
 }
